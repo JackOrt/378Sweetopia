8e3f17eca30db8af69d6a2075a87deb9eec9e0bc
Piper Feldman <stars@Pipers-MBP-3.attlocal.net>
2023-05-10 19:06:28 -0700

    hopefully fixed variables

--- Assets/spring.cs
+++ Assets/spring.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class spring : MonoBehaviour
 {
     public GameObject player;
-    public PlayerMovement pressed;
     // Start is called before the first frame update
     void Start()
     {
